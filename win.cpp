5dfb4763736db19a49da5fca8c911f786a8e365f
lekksha_ <lekksha@gmail.com>
2024-03-22 19:01:15 +0700

    add comments

--- win.cpp
+++ win.cpp
@@ -2,83 +2,91 @@
 
 #include <QMessageBox>
 #include <QVBoxLayout>
+
 Win::Win(QWidget *parent) : QWidget(parent)
 {
-    setWindowTitle("Возведение в квадрат");
-    frame = new QFrame(this);
-    frame->setFrameShadow(QFrame::Raised);
-    frame->setFrameShape(QFrame::Panel);
+    setWindowTitle("Возведение в квадрат"); // поставить название окна
+    frame = new QFrame(this);   // создать рамку
+    frame->setFrameShadow(QFrame::Raised);  // задать тень рамки
+    frame->setFrameShape(QFrame::Panel);    // задать форму рамки
     inputLabel = new QLabel("Введите число:",
-                            this);
-    inputEdit = new QLineEdit("",this);
-    StrValidator *v=new StrValidator(inputEdit);
-    inputEdit->setValidator(v);
-    outputLabel = new QLabel("Результат:",
+                            this);  // создать текст и передать его в this
+    inputEdit = new QLineEdit("",this); // создать текстовое поле и передать его в this
+    StrValidator *v = new StrValidator(inputEdit);  // создать StrValidator с родителем inputEdit
+    inputEdit->setValidator(v); // задать валидатор v для inputEdit
+    outputLabel = new QLabel("Результат:",  // создать текст и передать его в this
                              this);
-    outputEdit = new QLineEdit("",this);
-    nextButton = new QPushButton("Следующее",
+    outputEdit = new QLineEdit("",this);    // создать текстовое поле и передать его в this
+    nextButton = new QPushButton("Следующее",   // создать кнопку и передать ее в this
                                  this);
-    exitButton = new QPushButton("Выход",
+    exitButton = new QPushButton("Выход",   // создать кнопку и передать ее в this
                                  this);
     // компоновка приложения выполняется согласно рисунку 2.
     QVBoxLayout *vLayout1 = new QVBoxLayout(frame);
-    vLayout1->addWidget(inputLabel);
-    vLayout1->addWidget(inputEdit);
-    vLayout1->addWidget(outputLabel);
-    vLayout1->addWidget(outputEdit);
-    vLayout1->addStretch();
+        // создать вертикальную компоновку и поместить ее в рамку
+    vLayout1->addWidget(inputLabel); // добавить в компоновку
+    vLayout1->addWidget(inputEdit); // добавить в компоновку
+    vLayout1->addWidget(outputLabel);   // добавить в компоновку
+    vLayout1->addWidget(outputEdit);    // добавить в компоновку
+    vLayout1->addStretch(); // "выталкививать" вверх вышедобавленные элем. с помощью пространства
     QVBoxLayout *vLayout2 = new QVBoxLayout();
-    vLayout2->addWidget(nextButton);
-    vLayout2->addWidget(exitButton);
-    vLayout2->addStretch();
+        // создать вертикальную компоновку и поместить ее в рамку
+    vLayout2->addWidget(nextButton);    // добавить в компоновку
+    vLayout2->addWidget(exitButton);    // добавить в компоновку
+    vLayout2->addStretch(); // "выталкививать" вверх вышедобавленные элем. с помощью пространства
     QHBoxLayout *hLayout = new QHBoxLayout(this);
-    hLayout->addWidget(frame);
-    hLayout->addLayout(vLayout2);
-    begin();
-    connect(exitButton,SIGNAL(clicked(bool)),
+        // создать горизонтальную компоновку и поместить ее в рамку
+    hLayout->addWidget(frame);  // добавить в компоновку
+    hLayout->addLayout(vLayout2);   // добавить в компоновку
+    begin();    // установить начальный интерфейс
+    connect(exitButton,SIGNAL(clicked(bool)),   // добавить логику для кнопки выхода
             this,SLOT(close()));
-    connect(nextButton,SIGNAL(clicked(bool)),
+    connect(nextButton,SIGNAL(clicked(bool)),   // добавить логику для кнопки следующего числа
             this,SLOT(begin()));
-    connect(inputEdit,SIGNAL(returnPressed()),
+    connect(inputEdit,SIGNAL(returnPressed()),  // добавить логику для кнопки вычисления значения
             this,SLOT(calc()));
 }
 
-void Win::begin()
+void Win::begin()   // метод начальной настройки интерфейса
 {
-    inputEdit->clear();
-    nextButton->setEnabled(false);
-    nextButton->setDefault(false);
-    inputEdit->setEnabled(true);
-    outputLabel->setVisible(false);
-    outputEdit->setVisible(false);
-    outputEdit->setEnabled(false);
+    inputEdit->clear(); // очистить поле ввода
+    nextButton->setEnabled(false);  // отключить кнопку
+    nextButton->setDefault(false);  // поставить нажатие на эту кнопку по умолчанию
+    inputEdit->setEnabled(true);    // включить поле ввода
+    outputLabel->setVisible(false); // сделать невидимым текст
+    outputEdit->setVisible(false);  // сделать невидимыым поле
+    outputEdit->setEnabled(false);  // сделать невозможным взаимодействие с полем
     inputEdit->setFocus();
+        // сделать ввод с клавиатуры в текстовом поле возможным сразу после запуска
 }
 
-void Win::calc()
+void Win::calc()    // метод для вычисления значения и обновления UI
 {
-    bool Ok=true; float r, a;
-    QString str=inputEdit->text();
-    a=str.toDouble(&Ok);
+    bool Ok = true; // инициализировать //TODO: true можно убрать
+    float r, a; // результат, введеное число
+    QString str = inputEdit->text();    // объявить переменную с значением поля ввода
+    a = str.toDouble(&Ok);    // если преобразование будет неуспешным Ok изменит значение
     if (Ok)
     {
-        r=a*a;
-        str.setNum(r);
-        outputEdit->setText(str);
-        inputEdit->setEnabled(false);
-        outputLabel->setVisible(true);
-        outputEdit->setVisible(true);
-        nextButton->setDefault(true);
-        nextButton->setEnabled(true);
-        nextButton->setFocus();
+        r = a * a;  // вычислить результат
+        str.setNum(r);  // присвоить переменной значение результата
+        outputEdit->setText(str);   // дать полю вывода значение переменной
+        inputEdit->setEnabled(false);   // включить кнопку
+        outputLabel->setVisible(true);  // сделать видимым
+        outputEdit->setVisible(true);   // сделать видимым
+        nextButton->setDefault(true);   // поставить нажатие на эту кнопку по умолчанию
+        nextButton->setEnabled(true);   // включить кнопку
+        nextButton->setFocus(); // поставить фокус на кнопку
     }
     else
-        if (!str.isEmpty())
+        if (!str.isEmpty()) // если строка не пустая
         {
-            QMessageBox msgBox(QMessageBox::Information,
-                               "Возведение в квадрат.",
-                               "Введено неверное значение.",
-                               QMessageBox::Ok);
+            QMessageBox msgBox(QMessageBox::Warning,    // Поставить иконку предупреждения
+                               "Возведение в квадрат.", //  Задать название окна
+                               "Введено неверное значение.",    // Описание ошибки
+                               QMessageBox::Ok);    // добавить кнопку ОК
+                // создать окно ошибки
             msgBox.exec();
+                // запустить окно ошибки
         }
 }
